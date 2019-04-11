e32d95d3345b25dd2fd14c4c013789aaa8f2b485
MartinLenglet <martinlenglet@gmail.com>
2019-04-11 15:26:42 +0200

    ajout charte graphique

--- src/java/com/bank/dao/ConnectConf.java
+++ src/java/com/bank/dao/ConnectConf.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class ConnectConf {
     
     protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-    protected static final String DB_NAME = "Banquajoel";
+    protected static final String DB_NAME = "banquajoel";
     protected static final String URL = "jdbc:mysql://localhost/" + DB_NAME;
     protected static final String USER = "root";
     protected static final String PASSWORD = "";
