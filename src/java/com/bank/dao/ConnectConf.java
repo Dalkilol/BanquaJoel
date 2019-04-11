a22a0177669c528d2085dee0c84c632aee1a5999
MartinLenglet <martinlenglet@gmail.com>
2019-04-11 15:20:21 +0200

    ajout connection bdd

--- src/java/com/bank/dao/ConnectConf.java
+++ src/java/com/bank/dao/ConnectConf.java
@@ -5,10 +5,34 @@
  */
 package com.bank.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author ESIC
  */
 public class ConnectConf {
+    
+    protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    protected static final String DB_NAME = "Banquajoel";
+    protected static final String URL = "jdbc:mysql://localhost/" + DB_NAME;
+    protected static final String USER = "root";
+    protected static final String PASSWORD = "";
+    
+    static {
+        try {
+            Class.forName(DRIVER_NAME).newInstance();
+            
+        } catch (Exception e) {
+             System.err.println(e.getMessage());
+        }
+           
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
     
 }
