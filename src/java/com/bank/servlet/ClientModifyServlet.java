6cb92281871cc2b2923d996466a2620e81aff920
stagiaire <stagiaire@192.168.1.76>
2019-04-12 12:54:07 +0200

    modif ClientDao

--- src/java/com/bank/servlet/ClientModifyServlet.java
+++ src/java/com/bank/servlet/ClientModifyServlet.java
@@ -61,7 +61,21 @@ public class ClientModifyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            
+            processRequest(request, response);
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String mdp = request.getParameter("mdp");
         String mail = request.getParameter("mail");
        
@@ -80,20 +94,6 @@ public class ClientModifyServlet extends HttpServlet {
             PrintWriter out = response.getWriter();
             out.println(e.getMessage());
         }
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
